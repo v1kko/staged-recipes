c365820c5296a440646e0320eb34df1edcc616cb
Victor Azizi <victor@lipsum.eu>
2021-04-22 21:11:02 +0200

    env variables

--- recipes/superlu_win/bld.bat
+++ recipes/superlu_win/bld.bat
@@ -6,9 +6,9 @@ if errorlevel 1 exit 1
 # enable_blaslib=OFF so OpenBLAS will be found instead of the built-in BLAS
 
 cmake .. ^
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" ^
-    -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" ^
-    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fPIC" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="%PREFIX%/lib" ^
+    -DCMAKE_C_FLAGS="%CMAKE_C_FLAGS% -fPIC" ^
     -Denable_blaslib=OFF ^
     -Denable_single=ON ^
     -Denable_double=ON ^
@@ -16,7 +16,8 @@ cmake .. ^
     -Denable_complex=ON ^
     -Denable_tests=ON ^
     -Denable_doc=OFF ^
-    -Denable_matlab_mex=OFF
+    -Denable_matlab_mex=OFF ^
+    -DXSDK_ENABLE_Fortran=FALSE
 if errorlevel 1 exit 1
 make
 if errorlevel 1 exit 1
